3b63b52cc78f4354f86596e46c7d190d15f1095e
Justin Hester <justinhester@users.noreply.github.com>
2018-06-10 11:18:19 -0700

    Chapter 19 (#28)
    * Added code listing from 19.1 GPIO I/O Memory.
    
    * Added code listings for 19.2 GPIO Connections.
    
    * Added solution for exercise 34.

---
+++ contents/exercises/exercises-34/blink5.s
@@ -0,0 +1,140 @@
+@ blink5.s
+@ Blinks LED five times every second.
+@ 2018-06-30: Justin Hester
+
+@ Define my Raspberry Pi
+	.cpu	cortex-a53
+	.fpu	neon-fp-armv8
+	.syntax	unified		@ modern syntax
+
+@ Constants for assembler
+@ The following are defined in /usr/include/asm-generic/fcntl.h:
+@ Note that the values are specified in octal.
+	.equ	O_RDWR,00000002		@ open for read/write
+	.equ	O_DSYNC,00010000	@ synchronize virtual memory
+	.equ	__O_SYNC,04000000	@ programming changes with
+	.equ	O_SYNC,__O_SYNC|O_DSYNC	@ I/O Memory
+@ The following are defined in /usr/include/asm-generic/mman-common.
+	.equ	PROT_READ,0x1	@ page can be read
+	.equ	PROT_WRITE,0x2	@ page can be written
+	.equ	MAP_SHARED,0x01	@ share changes
+@ The following are define by me:
+	.equ	PERIPH, 0x3f000000	@ RPi 2 & 3 peripherals
+@	.equ	PERIPH, 0x20000000	@ RPi zero & 1 peripherals
+	.equ	GPIO_OFFSET,0x200000	@ start of GPIO device
+	.equ	O_FLAGS,O_RDWR|O_SYNC	@ open file flags
+	.equ	PROT_RDWR,PROT_READ|PROT_WRITE
+	.equ	NO_PREF,0
+	.equ	PAGE_SIZE,4096		@ Raspbian memory page
+	.equ	FILE_DESCRP_ARG,0	@ file descriptor
+	.equ	DEVICE_ARG,4		@ device address
+	.equ	STACK_ARGS,8		@ sp already 8-byte aligned
+	.equ	PIN_NO,17		@ bcm pin connected to negative side of led
+	.equ	OUTPUT,1		@ sets pin function to output mode
+
+@ Constant program data
+	.section	.rodata
+	.align	2
+device:
+	.asciz	"/dev/gpiomem"
+fdMsg:
+	.asciz	"File descriptor = %i\n"
+memMsg:
+	.asciz	"Using memory at %p\n"
+
+@ The program
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	sub	sp, sp, 24	@ space for saving regs
+	str	r4, [sp, 0]	@ save r4
+	str	r5, [sp, 4]	@	r5
+	str	r6, [sp, 8]	@	r6
+	str	fp, [sp, 12]	@	fp
+	str	lr, [sp, 16]	@	lr
+	add	fp, sp, 16	@ set our frame pointer
+	sub	sp, sp, STACK_ARGS	@ sp on 8-byte boundary
+
+@ Open /dev/gpiomem for read/write and syncing
+	ldr	r0, deviceAddr	@ address of /dev/gpiomem
+	ldr	r1, openMode	@ flags for accessing device
+	bl	open
+	mov	r4, r0		@ use r4 for file descriptor
+
+@ Display file descriptor
+	ldr	r0, fdMsgAddr	@ address of /dev/gpiomem
+	mov	r1, r4		@ file descriptor
+	bl	printf
+
+@ Map the GPIO registers to a virtual memory location so we can access them
+	str	r4, [sp, FILE_DESCRP_ARG]	@ /dev/gpiomem file descriptor
+	ldr	r0, gpio	@ address of GPIO
+	str	r0, [sp, DEVICE_ARG]	@ location of GPIO
+	mov	r0, NO_PREF	@ let kernel pick memory
+	mov	r1, PAGE_SIZE	@ get 1 page of memory
+	mov	r2, PROT_RDWR	@ read/write this memory
+	mov	r3, MAP_SHARED	@ share with other processes
+	bl	mmap
+	mov	r5, r0		@ save virtual memory address
+
+@ Display virtual address
+	mov	r1, r5
+	ldr	r0, memMsgAddr
+	bl	printf
+
+@ Set pin function
+	mov	r0, r5
+	mov	r1, PIN_NO
+	mov	r2, OUTPUT
+	bl	gpioPinFSelect
+
+@ Call gpioFSet and gpioFClr to activate/deactivate the LED five times.
+	mov	r6, 5		@ loop control variable
+blinks:
+	cmp	r6, 0		@ check if lcv has reached zero
+	beq	done		@ if so, exit loop and enter epilogue.
+	mov	r0, r5
+	mov	r1, PIN_NO
+	bl	gpioPinClr
+	mov	r0, 1		@ otherwise, blink led.
+	bl	sleep
+	mov	r0, r5
+	mov	r1, PIN_NO
+	bl	gpioPinSet
+	mov	r0, 1
+	bl	sleep
+	sub	r6, r6, 1	@ decrement loop control variable
+	b	blinks
+done:
+	mov	r0, r5		@ memory to unmap
+	mov	r1, PAGE_SIZE	@ amount we mapped
+	bl	munmap		@ unmap it
+
+	mov	r0, r4		@ /dev/gpiomem file descriptor
+	bl	close		@ close the file
+
+	mov	r0, 0		@ return 0;
+	add	sp, sp, STACK_ARGS	@ fix sp
+	ldr	r4, [sp, 0]	@ restore r4
+	ldr	r5, [sp, 4]	@	r5
+	ldr	r6, [sp, 8]
+	ldr	fp, [sp, 12]	@	fp
+	ldr	lr, [sp, 16]	@ 	lr
+	add	sp, sp, 24	@ restore sp
+	bx	lr		@ return
+
+	.align	2
+@ addresses of messages
+fdMsgAddr:
+	.word	fdMsg
+deviceAddr:
+	.word	device
+openMode:
+	.word	O_FLAGS
+memMsgAddr:
+	.word	memMsg
+gpio:
+	.word	PERIPH+GPIO_OFFSET
+
---
+++ contents/exercises/exercises-34/exercises-34.md
@@ -0,0 +1,6 @@
+# General Purpose Input/Output (GPIO) Device
+
+1. Write an assembly language program that blinks an LED five times. The LED will be on for 1 second then off for 1 second. Use the `sleep` function for timing, which is described in `man 3 sleep`.
+
+  **See `blink5` target in Makefile.**
+
